41b0e3a32eb62756afee3d0c7208bdf072289d94
Christopher Vogt <oss.nsp@cvogt.org>
2017-03-27 19:16:47 -0400

    prevent classClassLoaders for builds, currently not supported currently leads to "you need to extend an appropriate Build class" exception, because BuildBuild has a different BaseBuild class than builds with a flat classloader

--- stage2/Plugin.scala
+++ stage2/Plugin.scala
@@ -2,4 +2,7 @@ package cbt
 trait Plugin extends BaseBuild{
   override def dependencies = super.dependencies :+ context.cbtDependency
   object plugins extends plugins
+
+  /** CBT relies on hierarchical classloaders */
+  final override def flatClassLoader = false
 }
